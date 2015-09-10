e8ad02c296653b0a129bafb178f9220941561a0c
duketwo <duketwoqq@gmail.com>
2015-09-10 14:22:22 +0000

    nuget package remove

--- Questor/QuestorUI.Designer.cs
+++ Questor/QuestorUI.Designer.cs
@@ -847,7 +847,7 @@
             this.tabControl2.Location = new System.Drawing.Point(2, 3);
             this.tabControl2.Name = "tabControl2";
             this.tabControl2.SelectedIndex = 0;
-            this.tabControl2.Size = new System.Drawing.Size(799, 500);
+            this.tabControl2.Size = new System.Drawing.Size(799, 460);
             this.tabControl2.TabIndex = 127;
             // 
             // tabPage3
@@ -878,7 +878,7 @@
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(791, 474);
+            this.tabPage3.Size = new System.Drawing.Size(791, 434);
             this.tabPage3.TabIndex = 1;
             this.tabPage3.Text = "Questor";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -6211,7 +6211,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
-            this.ClientSize = new System.Drawing.Size(801, 501);
+            this.ClientSize = new System.Drawing.Size(801, 467);
             this.Controls.Add(this.tabControl2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
