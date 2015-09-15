f31bd6fffa2d3b033e0884bf68908e76c7c1e34e
duketwo <duketwoqq@gmail.com>
2015-09-15 03:06:42 +0000

    agentinteraction fix

--- Questor.Modules/Actions/Arm.cs
+++ Questor.Modules/Actions/Arm.cs
@@ -1058,6 +1058,7 @@ namespace Questor.Modules.Actions
 				if (SwitchShipsOnly)
 				{
 					ChangeArmState(ArmState.Done, true);
+					return true;
 				}
 				
 				ChangeArmState(ArmState.RepairShop, true);
