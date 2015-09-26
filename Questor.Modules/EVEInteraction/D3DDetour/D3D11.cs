7fa5ee781b127384c8ad3dcaa7436dcb97bd8bfa
duketwo <duketwoqq@gmail.com>
2015-09-26 17:22:07 +0000

    dx 11 support

--- Questor.Modules/EVEInteraction/D3DDetour/D3D11.cs
+++ Questor.Modules/EVEInteraction/D3DDetour/D3D11.cs
@@ -1,121 +1,113 @@
-﻿using System;
+﻿using EasyHook;
+using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using EasyHook;
-
 
 namespace D3DDetour
 {
-public class D3D11 : D3DHook
-	{
-		public struct SwapChainDescription
-		{
-			public D3D11.ModeDescription ModeDescription;
-			public D3D11.SampleDescription SampleDescription;
-			public int Usage;
-			public int BufferCount;
-			public IntPtr OutputHandle;
-			[MarshalAs(UnmanagedType.Bool)]
-			public bool IsWindowed;
-			public int SwapEffect;
-			public int Flags;
-		}
-		public struct Rational
-		{
-			public int Numerator;
-			public int Denominator;
-		}
-		public struct ModeDescription
-		{
-			public int Width;
-			public int Height;
-			public D3D11.Rational RefreshRate;
-			public int Format;
-			public int ScanlineOrdering;
-			public int Scaling;
-		}
-		public struct SampleDescription
-		{
-			public int Count;
-			public int Quality;
-		}
-		private delegate void Delegate0(IntPtr intptr_0);
-		private delegate int Delegate1(int int_0, int int_1, int int_2);
-		private D3D11.Delegate1 delegate1_0;
-		private LocalHook localHook_0;
-		public IntPtr EndScenePointer = IntPtr.Zero;
-		public IntPtr ResetPointer = IntPtr.Zero;
-		public IntPtr ResetExPointer = IntPtr.Zero;
-		private IntPtr intptr_0;
-		private IntPtr intptr_1;
-		private IntPtr intptr_2;
-		public unsafe override void Initialize()
-		{
-			Form form = new Form();
-			D3D11.SwapChainDescription swapChainDescription = new D3D11.SwapChainDescription
-			{
-				BufferCount = 1,
-				ModeDescription = new D3D11.ModeDescription
-				{
-					Format = 28
-				},
-				Usage = 32,
-				OutputHandle = form.Handle,
-				SampleDescription = new D3D11.SampleDescription
-				{
-					Count = 1
-				},
-				IsWindowed = true
-			};
-			IntPtr zero = IntPtr.Zero;
-			IntPtr zero2 = IntPtr.Zero;
-			IntPtr zero3 = IntPtr.Zero;
-		D3D11CreateDeviceAndSwapChain((void*)IntPtr.Zero, 1, (void*)IntPtr.Zero, 0, (void*)IntPtr.Zero, 0, 7, (void*)(&swapChainDescription), (void*)(&zero), (void*)(&zero2), (void*)IntPtr.Zero, (void*)(&zero3));
-			this.intptr_0 = zero;
-			this.intptr_1 = zero2;
-			this.intptr_2 = zero3;
-			IntPtr intPtr = Marshal.ReadIntPtr(Marshal.ReadIntPtr(this.intptr_0), 32);
-			D3D11.Delegate0 @delegate = (D3D11.Delegate0)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(this.intptr_0), 8), typeof(D3D11.Delegate0));
-			D3D11.Delegate0 delegate2 = (D3D11.Delegate0)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(this.intptr_1), 8), typeof(D3D11.Delegate0));
-			D3D11.Delegate0 delegate3 = (D3D11.Delegate0)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(this.intptr_2), 8), typeof(D3D11.Delegate0));
-			@delegate(this.intptr_0);
-			delegate2(this.intptr_1);
-			delegate3(this.intptr_2);
-			this.delegate1_0 = (D3D11.Delegate1)Marshal.GetDelegateForFunctionPointer(intPtr, typeof(D3D11.Delegate1));
-			this.localHook_0 = LocalHook.Create(intPtr, new D3D11.Delegate1(this.method_0), this);
-			
-			int[] exclusiveACL = new int[1];
-			localHook_0.ThreadACL.SetExclusiveACL(exclusiveACL);
-		}
-		private int method_0(int int_0, int int_1, int int_2)
-		{
-			base.RaiseEvent();
-			return this.delegate1_0(int_0, int_1, int_2);
-		}
-		public override void Remove()
-		{
-			this.localHook_0.Dispose();
-		}
-		[DllImport("d3d11.dll")]
-		static extern unsafe int D3D11CreateDeviceAndSwapChain(void* pVoid_0, int int_0, void* pVoid_1, int int_1, void* pVoid_2, int int_2, int int_3, void* pVoid_3, void* pVoid_4, void* pVoid_5, void* pVoid_6, void* pVoid_7);
-		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-		public static extern IntPtr GetModuleHandle(string lpModuleName);
-		[DllImport("kernel32.dll", CharSet = CharSet.Ansi)]
-		public static extern IntPtr LoadLibraryA(IntPtr lpModuleName);
-		public static void LoadLibrary(string libraryName)
-		{
-			if (D3D11.GetModuleHandle(libraryName) == IntPtr.Zero)
-			{
-				IntPtr intPtr = Marshal.StringToHGlobalAnsi(libraryName);
-				try
-				{
-					D3D11.LoadLibraryA(intPtr);
-				}
-				finally
-				{
-					Marshal.FreeHGlobal(intPtr);
-				}
-			}
-		}
-	}
+    public class D3D11 : D3DHook
+    {
+        private Direct3D11Present _presentDelegate;
+        private LocalHook Hook;
+
+        public override unsafe void Initialize()
+        {
+            Form form = new Form();
+            SwapChainDescription chainDescription = new SwapChainDescription()
+            {
+                BufferCount = 1,
+                ModeDescription = new ModeDescription()
+                {
+                    Format = 28
+                },
+                Usage = 32,
+                OutputHandle = form.Handle,
+                SampleDescription = new SampleDescription()
+                {
+                    Count = 1
+                },
+                IsWindowed = true
+            };
+
+            IntPtr ppSwapChain = IntPtr.Zero;
+            IntPtr ppDevice = IntPtr.Zero;
+            IntPtr ppImmediateContext = IntPtr.Zero;
+            IntPtr pFeatureLevel = IntPtr.Zero;
+            int ret = D3D11CreateDeviceAndSwapChain(IntPtr.Zero, 1, IntPtr.Zero, 0, IntPtr.Zero, 0, 7, &chainDescription, out ppSwapChain, out ppDevice, out pFeatureLevel, out ppImmediateContext);
+            IntPtr ptr = Marshal.ReadIntPtr(Marshal.ReadIntPtr(ppSwapChain), 32);
+            D3DVirtVoid d3dChain = (D3DVirtVoid)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(ppSwapChain), 8), typeof(D3DVirtVoid));
+            D3DVirtVoid d3dDevice = (D3DVirtVoid)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(ppDevice), 8), typeof(D3DVirtVoid));
+            D3DVirtVoid d3dContext = (D3DVirtVoid)Marshal.GetDelegateForFunctionPointer(Marshal.ReadIntPtr(Marshal.ReadIntPtr(ppImmediateContext), 8), typeof(D3DVirtVoid));
+            d3dChain(ppSwapChain);
+            d3dDevice(ppDevice);
+            d3dContext(ppImmediateContext);
+            _presentDelegate = (Direct3D11Present)Marshal.GetDelegateForFunctionPointer(ptr, typeof(Direct3D11Present));
+            Hook = LocalHook.Create(ptr, (Delegate)new Direct3D11Present(Callback), (object)this);
+            Hook.ThreadACL.SetExclusiveACL(new int[1]);
+        }
+
+        private int Callback(IntPtr swapChainPtr, int syncInterval, int flags)
+        {
+            RaiseEvent();
+            return _presentDelegate(swapChainPtr, syncInterval, flags);
+        }
+
+        public override void Remove()
+        {
+            Hook.Dispose();
+        }
+
+        [DllImport("d3d11.dll")]
+        private static extern unsafe int D3D11CreateDeviceAndSwapChain(IntPtr pAdapter, int driverType, IntPtr Software,
+        int flags, IntPtr pFeatureLevels,
+        int FeatureLevels, int SDKVersion,
+        void* pSwapChainDesc, [Out] out IntPtr ppSwapChain,
+        [Out] out IntPtr ppDevice, [Out] out IntPtr pFeatureLevel,
+        [Out] out IntPtr ppImmediateContext);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate void D3DVirtVoid(IntPtr istance);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate int Direct3D11Present(IntPtr swapChainPtr, int syncInterval, int flags);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SwapChainDescription
+        {
+            public ModeDescription ModeDescription;
+            public SampleDescription SampleDescription;
+            public int Usage;
+            public int BufferCount;
+            public IntPtr OutputHandle;
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool IsWindowed;
+            public int SwapEffect;
+            public int Flags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Rational
+        {
+            public int Numerator;
+            public int Denominator;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ModeDescription
+        {
+            public int Width;
+            public int Height;
+            public Rational RefreshRate;
+            public int Format;
+            public int ScanlineOrdering;
+            public int Scaling;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SampleDescription
+        {
+            public int Count;
+            public int Quality;
+        }
+    }
 }
--- Questor.Modules/EVEInteraction/StandAlone.cs
+++ Questor.Modules/EVEInteraction/StandAlone.cs
@@ -1,35 +1,55 @@
 ﻿namespace DirectEve
 {
-    using System;
-    using D3DDetour;
-    using System.Diagnostics;
+	using System;
+	using D3DDetour;
+	using System.Diagnostics;
+	using System.Runtime.InteropServices;
 
-    public class StandaloneFramework : IFramework
-    {
-        private EventHandler<EventArgs> _frameHook = null;
+	public class StandaloneFramework : IFramework
+	{
+		private EventHandler<EventArgs> _frameHook = null;
+		
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        public void RegisterFrameHook(EventHandler<EventArgs> frameHook)
-        {
-            Pulse.Initialize(D3DVersion.Direct3D9);
-            _frameHook = frameHook;
-            D3DHook.OnFrame += _frameHook;
-        }
+		public void RegisterFrameHook(EventHandler<EventArgs> frameHook)
+		{
+			
+			
+			IntPtr d3D9Loaded = IntPtr.Zero;
+			IntPtr d3D11Loaded = IntPtr.Zero;
 
-        public void RegisterLogger(EventHandler<EventArgs> logger)
-        {
-        }
+			d3D9Loaded = GetModuleHandle("d3d9.dll");
+			d3D11Loaded = GetModuleHandle("d3d11.dll");
 
-        public void Log(string msg)
-        {
-            Debugger.Log(0, "", msg);
-        }
+			if (d3D11Loaded != IntPtr.Zero)
+			{
+				Pulse.Initialize(D3DVersion.Direct3D11);
+			}
+			else
+			{
+				Pulse.Initialize(D3DVersion.Direct3D9);
+			}
+			
+			_frameHook = frameHook;
+			D3DHook.OnFrame += _frameHook;
+		}
 
-        #region IDisposable Members
-        public void Dispose()
-        {
-            D3DHook.OnFrame -= _frameHook;
-            Pulse.Shutdown();
-        }
-        #endregion
-    }
+		public void RegisterLogger(EventHandler<EventArgs> logger)
+		{
+		}
+
+		public void Log(string msg)
+		{
+			Debugger.Log(0, "", msg);
+		}
+
+		#region IDisposable Members
+		public void Dispose()
+		{
+			D3DHook.OnFrame -= _frameHook;
+			Pulse.Shutdown();
+		}
+		#endregion
+	}
 }
