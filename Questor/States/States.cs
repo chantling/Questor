0e3fd0b5a7b0f5f94bf72b217620949d73c95a1d
duketwo <duketwoqq@gmail.com>
2015-10-08 20:06:58 +0000

    removed CourierMissionCtrlState

--- Questor/States/States.cs
+++ Questor/States/States.cs
@@ -38,8 +38,6 @@ namespace Questor.Modules.States
 
         public static DebugHangarsBehaviorState CurrentDebugHangarBehaviorState { get; set; }
 
-        public static CourierMissionCtrlState CurrentCourierMissionCtrlState { get; set; }
-
         public static CombatMissionCtrlState CurrentCombatMissionCtrlState { get; set; }
 
         //public static ActionState CurrentActionState { get; set; }
