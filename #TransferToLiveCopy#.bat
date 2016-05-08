1a750b9a8b7195b6b80d9c728fb69fd18e6a8f79
duketwo <seviers@gmail.com>
2016-05-08 17:43:53 +0200

    fix #TransferToLiveCopy#.bat

--- #TransferToLiveCopy#.bat
+++ #TransferToLiveCopy#.bat
@@ -4,43 +4,43 @@ set debug=false
 set scripturl=%~dp0
 set scripturl=%scripturl:~0,-1%
 
-:setinnerspacedotnetdirectory
+:setquestorDirectory
 if "%debug%"=="true" pause && echo ------------------------------------------ && echo ------------------------------------------
-if exist ".\..\..\Questor\" set innerspacedotnetdirectory=.\..\..\Questor\
-if not exist "%Innerspacedotnetdirectory%" goto :error
+if exist ".\..\..\Questor\" set questorDirectory=.\..\..\Questor\
+if not exist "%questorDirectory%" goto :error
 
 :CopyQuestor
 @Echo.
-@Echo Starting to copy EasyHook files from [.\DirectEVE\easyhook*.dll] to [.\output\]
+@Echo Starting to copy libsfiles from [.\libs\*.*] to [.\output\]
 @Echo on
-copy /y ".\DirectEVE\easyhook*.dll" ".\output\"
+copy /y ".\libs\*.*" ".\output\"
 
 @Echo.
-@Echo Starting to copy debug files from [.\output\*.pdb] to [%innerspacedotnetdirectory%]
+@Echo Starting to copy debug files from [.\output\*.pdb] to [%questorDirectory%]
 @Echo on
-copy /y ".\output\*.pdb" "%innerspacedotnetdirectory%"
+copy /y ".\output\*.pdb" "%questorDirectory%"
 @Echo off
 @Echo.
 set RandomNumber=%Random%
 @Echo Rename prior Quester.exe to Questor.exe.RandomNumber.questorold
-ren "%innerspacedotnetdirectory%\Questor.exe" "Questor.exe.%RandomNumber%.questorold"
+ren "%questorDirectory%\Questor.exe" "Questor.exe.%RandomNumber%.questorold"
 @Echo off
 @Echo Rename prior Utility.dll to Utility.dll.RandomNumber.questorold
-ren "%innerspacedotnetdirectory%\Utility.dll" "Utility.dll.%RandomNumber%.questorold"
+ren "%questorDirectory%\Utility.dll" "Utility.dll.%RandomNumber%.questorold"
 @Echo off
 @Echo.
 @Echo.@Echo Trying to delete all .questorold files
-Del /Q "%innerspacedotnetdirectory%\*.questorold"
+Del /Q "%questorDirectory%\*.questorold"
 @Echo off
 @Echo.
-@Echo Starting to copy DLL files from [.\output\*.dll] to [%innerspacedotnetdirectory%]
+@Echo Starting to copy DLL files from [.\output\*.dll] to [%questorDirectory%]
 @Echo on
-copy /y ".\output\*.dll" "%innerspacedotnetdirectory%"
+copy /y ".\output\*.dll" "%questorDirectory%"
 @Echo off
 @Echo.
-@Echo Starting to copy EXE files from [.\output\*.exe] to [%innerspacedotnetdirectory%]
+@Echo Starting to copy EXE files from [.\output\*.exe] to [%questorDirectory%]
 @Echo on
-copy /y ".\output\*.exe" "%innerspacedotnetdirectory%"
+copy /y ".\output\*.exe" "%questorDirectory%"
 @Echo.
 @Echo off
 
@@ -50,26 +50,26 @@ if "%debug%"=="true" Echo on
 
 :CopyXMLConfigFiles
 @Echo.
-@Echo *** always copy the template settings.xml file to [%innerspacedotnetdirectory%]
+@Echo *** always copy the template settings.xml file to [%questorDirectory%]
 @Echo on
-copy /y ".\output\settings.xml" "%innerspacedotnetdirectory%"
+copy /y ".\output\settings.xml" "%questorDirectory%"
 @Echo off
 @Echo.
-@Echo *** always copy factions.xml file to [%innerspacedotnetdirectory%]
-copy /y ".\output\factions.xml" "%innerspacedotnetdirectory%"
+@Echo *** always copy factions.xml file to [%questorDirectory%]
+copy /y ".\output\factions.xml" "%questorDirectory%"
 @Echo.
 @Echo *** only copy ShipTargetValues.xml if one does not already exist (it contains targeting data)
-if not exist "%innerspacedotnetdirectory%\ShipTargetValues.xml" copy /y ".\output\ShipTargetValues.xml" "%innerspacedotnetdirectory%"
+if not exist "%questorDirectory%\ShipTargetValues.xml" copy /y ".\output\ShipTargetValues.xml" "%questorDirectory%"
 @Echo.
 @Echo *** only copy invtypes.xml if one does not already exist (it contains pricing data)
-if not exist "%innerspacedotnetdirectory%\invtypes.xml" copy /y ".\output\invtypes.xml" "%innerspacedotnetdirectory%"
+if not exist "%questorDirectory%\invtypes.xml" copy /y ".\output\invtypes.xml" "%questorDirectory%"
 @Echo.
 @Echo *** only copy InvIgnore.xml if one does not already exist (it contains invtypes that will not be sold by valuedump)
-if not exist "%innerspacedotnetdirectory%\InvIgnore.xml" copy /y ".\output\InvIgnore.xml" "%innerspacedotnetdirectory%"
+if not exist "%questorDirectory%\InvIgnore.xml" copy /y ".\output\InvIgnore.xml" "%questorDirectory%"
 @Echo.
 @Echo. 
 @Echo *** only copy Skill_Prerequisites.xml if one does not already exist (it contains skill prerequisites which allows questor to determine which skills can be injected)
-if not exist "%innerspacedotnetdirectory%\Skill_Prerequisites.xml" copy /y ".\output\Skill_Prerequisites.xml" "%innerspacedotnetdirectory%"
+if not exist "%questorDirectory%\Skill_Prerequisites.xml" copy /y ".\output\Skill_Prerequisites.xml" "%questorDirectory%"
 @Echo off
 
 goto :done
@@ -79,4 +79,4 @@ echo ------------------------------------------ && echo ------------------------
 echo Error
 echo ------------------------------------------ && echo ------------------------------------------
 :done
-echo [done copying questor related files to: %innerspacedotnetdirectory%]
+echo [done copying questor related files to: %questorDirectory%]
