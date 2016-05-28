93f87b85da5cd63bc3e1357b1ce6ea2a2fb8bb5f
duketwo <seviers@gmail.com>
2016-05-28 17:25:29 +0200

    added abstract controller classes for upcoming changes

---
+++ Questor/Controllers/BaseController.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: duketwo
+ * Date: 28.05.2016
+ * Time: 17:07
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Questor.Controllers
+{
+	/// <summary>
+	/// Description of BaseController.
+	/// </summary>
+	public abstract class BaseController : IController
+	{
+		protected Random Rnd { get; set; }
+		protected int RandomFactor { get; set; }
+		public bool IsWorkDone { get; set; }
+		protected DateTime LocalPulse { get; set; }
+		
+		protected BaseController()
+		{
+			Rnd = new Random();
+			RandomFactor = 1;
+		}
+		
+		protected bool CanWork { get { return DateTime.UtcNow > LocalPulse } }
+
+		public abstract void DoWork();
+
+		protected int GetRandom(int minValue, int maxValue)
+		{
+			return Rnd.Next(minValue, maxValue) * RandomFactor;
+		}
+		protected DateTime GetUTCNowDelaySeconds(int minDelayInSeconds, int maxDelayInSeconds)
+		{
+			return DateTime.UtcNow.AddMilliseconds(GetRandom(minDelayInSeconds * 1000, maxDelayInSeconds * 1000));
+		}
+		
+		protected DateTime GetUTCNowDelayMilliseconds(int minDelayInMilliseconds, int maxDelayInMilliseconds)
+		{
+			return DateTime.UtcNow.AddMilliseconds(GetRandom(minDelayInMilliseconds, maxDelayInMilliseconds));
+		}
+		
+		internal int VeryLowRandom {
+			get { return GetRandom(500,1000)*RandomFactor; }
+		}
+		
+		internal int LowRandom {
+			get { return GetRandom(1000,2000)*RandomFactor; }
+		}
+		
+		internal int MediumRandom {
+			get {  return GetRandom(3500,5000)*RandomFactor; }
+		}
+		
+		internal int HighRandom {
+			get { return GetRandom(5000,10000)*RandomFactor; }
+		}
+	}
+}
---
+++ Questor/Controllers/IController.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: duketwo
+ * Date: 28.05.2016
+ * Time: 17:08
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Questor.Controllers
+{
+	/// <summary>
+	/// Description of IController.
+	/// </summary>
+  public interface IController
+    {
+        bool IsWorkDone { get; set; }
+        void DoWork();
+    }
+}
